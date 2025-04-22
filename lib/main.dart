9a75dc387bf9635ce517eda3772bee627a3a64ad
ahmed <am7405782@gmail.com>
2025-04-22 20:01:45 +0200

    Add Bloc providers for layout and profile management
    Introduce `LayoutCubit` and `ProfileCubit` to manage the state of the layout and profile features respectively. The `ProfileCubit` is initialized with `ProfileRepoImp` and fetches the profile data upon creation. These providers are wrapped in a `MultiBlocProvider` to ensure they are available throughout the app. This change enhances the app's state management capabilities and prepares for future feature integrations.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:goal_master_admin/core/components/preference_utility.dart';
 import 'package:goal_master_admin/core/routing/app_router.dart';
 import 'package:goal_master_admin/core/services/service_locator.dart';
+import 'package:goal_master_admin/features/layout/presentation/manager/layout_cubit.dart';
+import 'package:goal_master_admin/features/profail/data/repo/profile_repo_imp.dart';
+import 'package:goal_master_admin/features/profail/presentation/manager/profile_cubit/profile_cubit.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // ✅ حل المشكلة
@@ -18,33 +22,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(390, 844),
-      child: GestureDetector(
-        onTap: () {
-          FocusScopeNode currentFocus = FocusScope.of(context);
-          if (!currentFocus.hasPrimaryFocus) {
-            currentFocus.unfocus();
-            FocusManager.instance.primaryFocus?.unfocus();
-          }
-        },
-        child: OKToast(
-          child: MaterialApp.router(
-            title: "Goal Master Admin",
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => LayoutCubit(),
+        ),
+        BlocProvider(
+          create: (context) => ProfileCubit(
+            getIt<ProfileRepoImp>(),
+          )..getProfile(),
+        ),
+      ],
+      child: ScreenUtilInit(
+        designSize: const Size(390, 844),
+        child: GestureDetector(
+          onTap: () {
+            FocusScopeNode currentFocus = FocusScope.of(context);
+            if (!currentFocus.hasPrimaryFocus) {
+              currentFocus.unfocus();
+              FocusManager.instance.primaryFocus?.unfocus();
+            }
+          },
+          child: OKToast(
+            child: MaterialApp.router(
+              title: "Goal Master Admin",
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              ),
+              debugShowCheckedModeBanner: false,
+              locale: const Locale('ar'),
+              supportedLocales: const [
+                Locale('ar'),
+              ],
+              localizationsDelegates: const [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              routerConfig: AppRouter.router,
             ),
-            debugShowCheckedModeBanner: false,
-            locale: const Locale('ar'),
-            supportedLocales: const [
-              Locale('ar'),
-            ],
-            localizationsDelegates: const [
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            routerConfig: AppRouter.router,
           ),
         ),
       ),
